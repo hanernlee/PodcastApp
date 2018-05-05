06385ed6c042eb0d7759cec13b3f51a492cc41fc
Christopher Lee <hanernlee@gmail.com>
2018-05-05 20:11:33 +1000

    Maintabbarcontroller

---
+++ PodcastApp/PodcastApp/MainTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabBarController.swift
+//  PodcastApp
+//
+//  Created by Christopher Lee on 5/5/18.
+//  Copyright © 2018 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    // MARK:- Setup Functions
+    
+    func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    // MARK:- Helper Functions
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
