c70dd640589c42bfa4cbd87231f0aeab1d2cc73f
Christopher Lee <hanernlee@gmail.com>
2018-05-11 23:14:23 +1000

    PlayerDetailView revamp

--- PodcastApp/PodcastApp/Views/PlayerDetailsView.swift
+++ PodcastApp/PodcastApp/Views/PlayerDetailsView.swift
@@ -12,6 +12,7 @@ class PlayerDetailsView: UIView {
     var episode: Episode! {
         didSet {
             titleLabel.text = episode.title
+            authorLabel.text = episode.author
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url, completed: nil)
         }
@@ -21,6 +22,12 @@ class PlayerDetailsView: UIView {
         self.removeFromSuperview()
     }
     
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var episodeImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 2
+        }
+    }
 }
