ff5b73197736cdb7298d628a38f7970b9aefd776
Christopher Lee <hanernlee@gmail.com>
2018-05-14 22:59:43 +1000

    Time Interval for Player

---
+++ PodcastApp/PodcastApp/Extensions/CMTime.swift
@@ -0,0 +1,20 @@
+//
+//  CMTime.swift
+//  PodcastApp
+//
+//  Created by Christopher Lee on 14/5/18.
+//  Copyright © 2018 Christopher Lee. All rights reserved.
+//
+
+import AVKit
+
+extension CMTime {
+    func toDisplayString() -> String {
+        let totalSeconds = Int(CMTimeGetSeconds(self))
+        let seconds = totalSeconds % 60
+        let minutes = totalSeconds / 60
+        let hours = totalSeconds / 60 / 60
+        let timeFormatString = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        return timeFormatString
+    }
+}
--- PodcastApp/PodcastApp/Views/PlayerDetailsView.swift
+++ PodcastApp/PodcastApp/Views/PlayerDetailsView.swift
@@ -31,6 +31,8 @@ class PlayerDetailsView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        observePlayerCurrentTime()
+        
         let time = CMTimeMake(1, 3)
         let times = [NSValue(time: time)]
         player.addBoundaryTimeObserver(forTimes: times, queue: .main) {
@@ -68,6 +70,10 @@ class PlayerDetailsView: UIView {
         }
     }
     
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    
     @objc func handlePlayPause() {
         if player.timeControlStatus == .paused {
             player.play()
@@ -80,7 +86,28 @@ class PlayerDetailsView: UIView {
         }
     }
     
+    // MARK:- Fileprivate Func
+    
     fileprivate let shrunkenTransform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+    
+    fileprivate func observePlayerCurrentTime() {
+        let interval = CMTimeMake(1, 2)
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { (time) in
+            self.currentTimeLabel.text = time.toDisplayString()
+            let durationTime = self.player.currentItem?.duration
+            self.durationLabel.text = durationTime?.toDisplayString()
+            
+            self.updateCurrentTimeSlider()
+        }
+    }
+    
+    fileprivate func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationSeconds = CMTimeGetSeconds(player.currentItem?.duration ?? CMTimeMake(1, 1))
+        let percentage = currentTimeSeconds / durationSeconds
+        
+        self.currentTimeSlider.value = Float(percentage)
+    }
     
     fileprivate func shrinkEpisodeImageView() {
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
